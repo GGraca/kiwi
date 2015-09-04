eb7f2774170e050ee8cc6def5bcfdb5086df45ed
Miguel Martins Duarte <miguel.duarte20@hotmail.com>
2015-09-04 04:24:35 +0100

    Fixes. Date not working, not my fault...

--- client/event.js
+++ client/event.js
@@ -2,6 +2,8 @@
 Template.event.helpers({
 	posts: function(){
 		console.log(Events.findOne({_id: this.eventId}).hashtags)
-		return Posts.find({ hashtags: {$in: Events.findOne({_id: this.eventId}).hashtags }} ).fetch()
+		return Posts.find({ hashtags: {$in: Events.findOne({_id: this.eventId}).hashtags }},
+						  { sort: {date: -1} }
+		).fetch()
 	}
 })
